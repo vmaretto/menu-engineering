ee394f02d572fee9dcd5b9bd1d3d38d061555b2e
Virgilio Maretto <info@posti.world>
2025-06-14 13:56:59 +0000

    Fix Tailwind build issue for Vercel

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+/** postcss.config.js */
 module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    // usa il plugin "tailwindcss" nativo
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
